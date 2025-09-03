74b4abaca03a89a7e2004205ff275a83a18b3455
rebecamacedosoares <rebecamssilva@gmail.com>
2025-09-03 09:38:11 -0300

    feat: Criação de app products e estrutura de modelos de dados da aplicação

---
+++ src/products/models.py
@@ -0,0 +1,25 @@
+# src/products/models.py
+from django.db import models
+# from django.contrib.auth.models import User
+
+class Product(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    target_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class PriceHistory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='price_history')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.product.name} - {self.price}"
